3881ea4594150485ee1d437301c4246c656c5b9e
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-05-02 13:40:39 -0400

    update options MDP

--- HW2/options_mdp.py
+++ HW2/options_mdp.py
@@ -7,7 +7,7 @@
 | Homework #2 - Question #1     |
 ---------------------------------
 
-This script is designed to represent the options problem outlines in question 1 of homework #2. 
+This script is designed to represent the options problem outlined in question #1 of homework #2 for ENGG 177. 
 
 I will create an MDP similar to the "Random MDP" created as a template for this course, and hopefully 
 it will yield realistic results when used in the provided backward induction algorithm. 
@@ -30,17 +30,36 @@ def random_MDP(n_T, n_S, n_A, r_min, r_max, seed=0):
         r_max= maximum reward
     """""
 
-    #Generate transition probabilities and rewards
-    P = np.empty((n_S, n_S, n_T, n_A)); P[:] = np.nan
-    r = np.empty((n_S, n_T, n_A)); r[:] = np.nan
+    # Initialize empty matrices for transition probabilities
+    P = np.empty((n_S, n_S, n_T, n_A))
+    P[:] = np.nan
+    
+     # Initialize empty matrices for rewards
+    r = np.empty((n_S, n_T, n_A))
+    r[:] = np.nan
 
+    # we step forward once for each epoch before we get to T
     for t in range(n_T):
+        # we take all the actions possible from the current state s_t
         for a in range(n_A):
-            np.random.seed(seed); seed += 1  # seeds for pseudo-random number generator
+
+            # if action == 0 then we exercise option, if action == 1 we continue
+
+            # seeds for pseudo-random number generator
+            np.random.seed(seed)
+            seed += 1  
+
             TPM = np.random.rand(n_S, n_S)
             TPM = TPM.T/TPM.sum(axis=1)
+            
+            # add the current time (t) and action (a) to the P matrix outside the loop 
             P[:, :, t, a] = TPM.T
+
+            # generate the reward for being in state s_t and taking action (a) 
             for s in range(n_S):
+                # how to we add the correct rewards here?? 
+                if a == 0: 
+                    r[s, t, a] = s
                 r[s, t, a] = np.random.randint(r_min, r_max)
 
     #Generate terminal rewards
