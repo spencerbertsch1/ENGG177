fcb47413067da3a2b1b8077bcd403b0f8cdf3579
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-05-20 13:34:24 -0400

    first_visit_mcc working build

--- HW3/first_vitit_mc.py
+++ HW3/first_vitit_mc.py
@@ -4,7 +4,7 @@
 | ENGG 177 - Homework #3             |
 | Dartmouth College                  |  
 | Spring 2022                        |
-| Spencer Bertsch                    |
+| Author: Spencer Bertsch            |
 --------------------------------------
 
 This file contains the code needed to build an environment representing a 5 state MDP and find an 
@@ -15,8 +15,12 @@ This file can be run from the command line by running: $ python3 first_visit_mc.
 
 import numpy as np
 import random
+random.seed = 42
 
 class Env():
+
+    # define class variables
+    verbose = False  # <-- set to true to see all the detailed outputs
 
     def __init__(self):
         self.transition_matrix = self.create_transition_matrix()
@@ -24,8 +28,8 @@ class Env():
         self.action = 0  # <-- here 0 represents "stay", 1 represents "leave"
         self.lamda = 0.9  # <-- discount rate of 0.9
         self.epsilon = 1
-        self.a_star = np.zeros(5)  # <-- we initialize the 'best action' a_star to 0 or "stay"
-        self.pi = np.full((5, 2), np.nan)
+        self.a_star = np.zeros(5, dtype=np.int32)  # <-- we initialize the 'best action' a_star to 0 or "stay"
+        self.pi = np.full((5, 2), 0.5)
 
 
     def check_env(self):
@@ -88,9 +92,11 @@ class Env():
         np.random.choice(np.arange(pi.shape[1]), p=pi[s[-1], :])
         """
 
-        choice = np.random.choice([0.95, 0.025, 0.025])
+        # here we want to sample pi to get the next action
+        action_probabilities: list = list(self.pi[self.state-1]) 
+        self.action = np.random.choice([0, 1], p=action_probabilities)
 
-        if self.action == 0:
+        if (self.action == 0) & (self.state != 5):
 
             # define the value of U drawn from a uniform distribution between 0 and 1, [0, 1]. 
             U = np.random.uniform(1,0)
@@ -126,10 +132,16 @@ class Env():
 
                     if (sj_prob_sum_to_k_minus_one < U) & (U <= sj_prob_sum_to_k):
                         self.state = k
+                        break
 
+        elif (self.action == 1) & (self.state != 5):
+            # here we set the state to 5 - an absorbing state, the game is over
+            self.state = 5
         else:
-            # here we set the state to 20 - an absorbing state, the game is over
-            self.state = 20
+            if self.verbose:
+                print('We\'re in state 5, continuing.')
+            else:
+                pass
 
     
     def get_next_action(self):
@@ -149,7 +161,7 @@ class Env():
             self.action = not self.a_star
     
 
-    def first_visit_mc(self):
+    def on_policy_first_visit_mcc(self):
         """
         First visit MC algorithm implementation
         """
@@ -172,7 +184,6 @@ class Env():
         g_sa: np.array = np.full((5, 2), 0)
         Q_hat_sa: np.array = np.full((5, 2), 0)
 
-
         # what should the value of T be? 
         T = 50
 
@@ -187,12 +198,24 @@ class Env():
                 action: bool = self.action
                 state: int = self.state
                 reward: int = self.state
-                print(f'Time: {t}, State: {state}, Action: {action}, Reward: {reward}')
+                if state == 5:
+                    reward = 20
+                if self.verbose: 
+                    print(f'Time: {t}, State: {state}, Action: {action}, Reward: {reward}')
                 episode_results[t] = [state, action, reward]
+                if state == 5:
+                    if self.verbose:
+                        print('Action \'leave\' was chosen so we\'re now in state 5. Stopping the episode.')
+                    # we need to randomize the state before we start the next episode
+                    self.state = np.random.choice([1, 2, 3, 4])
+                    break
 
             G = 0
+            if self.verbose: 
+                print(f'Current Episode: {episode_results}')
             # we now move backward from t=T to t=0 to find all the problem parameters
-            for t in range(T-1, 0, -1):
+            episode_length: int = max([k for k, _ in episode_results.items()])
+            for t in range(episode_length, 0, -1):
                 # get the reward from the episode results
                 state_t = episode_results[t][0]
                 action_t = episode_results[t][1]
@@ -201,23 +224,23 @@ class Env():
                 # update G
                 G = (self.lamda * G) + reward_t
 
-                if not visited_states[state_t][action_t]: 
-                    print(f'First time visiting state {state_t} in episode {n}!')
+                if not visited_states[state_t-1][action_t]: 
+                    if self.verbose:
+                        print(f'First time visiting state {state_t} in episode {n}!')
                     # this is the first time we have visited this state in this episode! 
 
                     # update Ns and gs
-                    N_sa[state_t][action_t] = N_sa[state_t][action_t] + 1
-                    g_sa[state_t][action_t] = g_sa[state_t][action_t] + G
+                    N_sa[state_t-1][action_t] = N_sa[state_t-1][action_t] + 1
+                    g_sa[state_t-1][action_t] = g_sa[state_t-1][action_t] + G
 
                     # update Q_hat_sa
-                    Q_hat_sa[state_t][action_t] = g_sa[state_t][action_t] / N_sa[state_t][action_t]
+                    Q_hat_sa[state_t-1][action_t] = g_sa[state_t-1][action_t] / N_sa[state_t-1][action_t]
 
                     # set this state to visited
-                    visited_states[state_t][action_t] = True
+                    visited_states[state_t-1][action_t] = True
 
                 self.a_star = np.argmax(Q_hat_sa, axis=1) # <-- get the action that maximizes Q_hat_sa here! 
                     
-
             # we perform these operations on every iteration of the episode n
             self.epsilon = 1/n
 
@@ -226,11 +249,15 @@ class Env():
                 self.pi[state][self.a_star[state]] = 1 - self.epsilon
                 other_action = 1 - self.a_star[state]
                 self.pi[state][other_action] = self.epsilon
-            
-            print('something')
 
+            if self.verbose: 
+                print(f'New Policy: {self.pi}')
+            
+        print(f'Final Policy Pi: \n {np.around(self.pi, decimals=3)}')
+        
 
 # some test code
 if __name__ == "__main__":
     env = Env()
-    env.first_visit_mc()
+    # env.check_env()
+    env.on_policy_first_visit_mcc()
